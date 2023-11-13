d84d02dbf3b9f4665dc69e3935264ed7aca3012d
infosolution <infosolution65>
2023-11-13 18:18:54 +0900

    실적처리 push

---
+++ app/src/main/java/com/erp/Taiyo/item/FileNoProcessListItem.java
@@ -0,0 +1,134 @@
+package com.erp.Taiyo.item;
+
+public class FileNoProcessListItem {
+    public String strChk, strFileNo , strActualQty ,strOpPoiseOrderSeq ,strOpUnitOrderSeq, strReleaseDate ,strItemDesc ,strWeekActualQty ,strRemark ,strSectionDesc,strJobNo
+            ,strSplitFlag ,strOpPoiseOrderId,strOpUnitOrderId ,strOperationId ,strOperationDesc ;
+
+    public String getStrReleaseDate() {
+        return strReleaseDate;
+    }
+
+    public void setStrReleaseDate(String strReleaseDate) {
+        this.strReleaseDate = strReleaseDate;
+    }
+
+    public String getStrItemDesc() {
+        return strItemDesc;
+    }
+
+    public void setStrItemDesc(String strItemDesc) {
+        this.strItemDesc = strItemDesc;
+    }
+
+    public String getStrWeekActualQty() {
+        return strWeekActualQty;
+    }
+
+    public void setStrWeekActualQty(String strWeekActualQty) {
+        this.strWeekActualQty = strWeekActualQty;
+    }
+
+    public String getStrRemark() {
+        return strRemark;
+    }
+
+    public void setStrRemark(String strRemark) {
+        this.strRemark = strRemark;
+    }
+
+    public String getStrSectionDesc() {
+        return strSectionDesc;
+    }
+
+    public void setStrSectionDesc(String strSectionDesc) {
+        this.strSectionDesc = strSectionDesc;
+    }
+
+    public String getStrJobNo() {
+        return strJobNo;
+    }
+
+    public void setStrJobNo(String strJobNo) {
+        this.strJobNo = strJobNo;
+    }
+
+    public String getStrSplitFlag() {
+        return strSplitFlag;
+    }
+
+    public void setStrSplitFlag(String strSplitFlag) {
+        this.strSplitFlag = strSplitFlag;
+    }
+
+    public String getStrOpPoiseOrderId() {
+        return strOpPoiseOrderId;
+    }
+
+    public void setStrOpPoiseOrderId(String strOpPoiseOrderId) {
+        this.strOpPoiseOrderId = strOpPoiseOrderId;
+    }
+
+    public String getStrOpUnitOrderId() {
+        return strOpUnitOrderId;
+    }
+
+    public void setStrOpUnitOrderId(String strOpUnitOrderId) {
+        this.strOpUnitOrderId = strOpUnitOrderId;
+    }
+
+    public String getStrOperationId() {
+        return strOperationId;
+    }
+
+    public void setStrOperationId(String strOperationId) {
+        this.strOperationId = strOperationId;
+    }
+
+    public String getStrOperationDesc() {
+        return strOperationDesc;
+    }
+
+    public void setStrOperationDesc(String strOperationDesc) {
+        this.strOperationDesc = strOperationDesc;
+    }
+
+    public String getStrChk() {
+        return strChk;
+    }
+
+    public void setStrChk(String strChk) {
+        this.strChk = strChk;
+    }
+
+    public String getStrFileNo() {
+        return strFileNo;
+    }
+
+    public void setStrFileNo(String strFileNo) {
+        this.strFileNo = strFileNo;
+    }
+
+    public String getStrActualQty() {
+        return strActualQty;
+    }
+
+    public void setStrActualQty(String strActualQty) {
+        this.strActualQty = strActualQty;
+    }
+
+    public String getStrOpPoiseOrderSeq() {
+        return strOpPoiseOrderSeq;
+    }
+
+    public void setStrOpPoiseOrderSeq(String strOpPoiseOrderSeq) {
+        this.strOpPoiseOrderSeq = strOpPoiseOrderSeq;
+    }
+
+    public String getStrOpUnitOrderSeq() {
+        return strOpUnitOrderSeq;
+    }
+
+    public void setStrOpUnitOrderSeq(String strOpUnitOrderSeq) {
+        this.strOpUnitOrderSeq = strOpUnitOrderSeq;
+    }
+}

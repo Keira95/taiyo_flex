eb6192dc77ff6f358ff5e04aac2606a246cc6b85
infosolution <infosolution65>
2023-11-24 09:20:15 +0900

    231223 잘못스캔시 포커스 이동 금지 및 정상 스캔시 수정완

--- app/src/main/java/com/erp/Taiyo/activity/SearchActivity.java
+++ app/src/main/java/com/erp/Taiyo/activity/SearchActivity.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -13,11 +16,25 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.erp.Taiyo.Dialog.LuOillerDialog;
+import com.erp.Taiyo.adapter.FileNoProcessAdapter;
+import com.erp.Taiyo.adapter.SearchListAdapter;
+import com.erp.Taiyo.item.FileNoProcessListItem;
+import com.erp.Taiyo.item.SearchListItem;
 
 import com.erp.Taiyo.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -32,7 +49,7 @@ public class SearchActivity extends AppCompatActivity {
     String filter = null;
     String strDate;
 
-    EditText t1FileNo, t1ItemDesc, t1OperaionDesc, t1TankScan, t1LiqidPersonDesc, t1RiqidStartTime, t1RiqidEndTime, t1PowderStartTime, t1PowderEndTime, t1WorkEndTime, t1WorkcenterCode, t1WorkcenterDesc, t1WorkcenterId, t1WorkStartTiem, t1TankLcode, t1LiqidPersonId, t1PowderPersonId, t1Job_id, t1OprationId, t1PowderPersonDesc, t1ModeFlag;
+    EditText t10WorkcenterDesc, t10WorkcenterId, t10WorkcenterCode, t10FileNo, t10OperationId;
     String strSobId = "70";
     String strOrgId = "701";
     String strAssembly = "PPMF2201";
@@ -50,8 +67,8 @@ public class SearchActivity extends AppCompatActivity {
     ListView lv_log;
     ListView lvPaldlet, lvInput;
 
-    Button btnt1save;
-    Button btnWorkStartTiem, btnRiqidStartTime, btnRiqidEndTime, btnPowderStartTime, btnPowderEndTime, btnWorkEndTime;
+    Button btntsave, btnWorkcenteLu;
+
     //키보드
 
     InputMethodManager imm;
@@ -59,6 +76,9 @@ public class SearchActivity extends AppCompatActivity {
 
     private boolean ScanModify = true;
     private boolean Mod_Flag = true;
+
+    SearchListAdapter searchListAdapter = new SearchListAdapter();
+    SearchListItem searchListItem = new SearchListItem();
 
 
     @Override
@@ -71,16 +91,21 @@ public class SearchActivity extends AppCompatActivity {
         strUserName = intent.getStringExtra("O_USER_NAME");
         strMenuDesc = intent.getStringExtra("TOP_MENU_DESC");
 
-
         mainLayout = findViewById(R.id.main_layout);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         tvUserName = (TextView) findViewById(R.id.user_nm);
         tvMenu = (TextView) findViewById(R.id.tv_menu);
 
+        t10WorkcenterDesc = (EditText) findViewById(R.id.et_t10_workcenter_desc);
+        t10WorkcenterId = (EditText) findViewById(R.id.et_t10_workcenter_id);
+        t10WorkcenterCode = (EditText) findViewById(R.id.et_t10_workcenter_code);
+        t10FileNo = (EditText) findViewById(R.id.et_t10_file_no);
+        t10OperationId = (EditText) findViewById(R.id.et_t10_operation_id);
 
-        btnt1save = (Button) findViewById(R.id.btn_t1_save);
+        btntsave = (Button) findViewById(R.id.btn_t10_search);
+        btnWorkcenteLu = (Button) findViewById(R.id.btn_t10_workcenter_lookup);
 
-
+        lvInput = (ListView)  findViewById(R.id.lv_search);
         auto = getSharedPreferences("appData_Log", Context.MODE_PRIVATE);
         //키보드 내리기
 
@@ -89,19 +114,44 @@ public class SearchActivity extends AppCompatActivity {
 
 
 
+        initializeToolbar();
 
+        t10FileNo.requestFocus();
 
+        t10FileNo.addTextChangedListener(new TextWatcher() {
 
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if(getCurrentFocus() == t10FileNo && !s.toString().isEmpty() && t10FileNo.getText().toString().equals("")){
+
+                    t10FileNo.setText(s.toString());
+                    t10WorkcenterId.requestFocus();
+                }else{
+                    return;
+                }
+            }
+        });
 
 
     }
+
 
 
     private String getNowDate() {
 
         long now = System.currentTimeMillis();
         Date date = new Date(now);
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.KOREAN);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss", Locale.KOREAN);
 
         String getTime = sdf.format(date);
 
@@ -149,10 +199,132 @@ public class SearchActivity extends AppCompatActivity {
         // 유저 정보 삽입.
         tvUserName.setText(strUserName);
 
+        btnWorkcenteLu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LuOillerDialog luOillerDialog = new LuOillerDialog(SearchActivity.this);
+                luOillerDialog.call_Workcetner(strIp ,t10WorkcenterCode,t10WorkcenterDesc,t10WorkcenterId,strUserId, t10OperationId);
+
+            }
+        });
+        btntsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SEARCH sEARCH = new SEARCH();
+                    sEARCH.execute(strIp, strSobId, strOrgId, t10WorkcenterId.getText().toString(), t10FileNo.getText().toString().replaceAll("\n", ""));
+
+            }
+        });
+
 
     }
 
 
+
+    // FILE_NO_SCAN
+    protected class SEARCH extends AsyncTask<String, Void, String> {
+        final SearchListAdapter searchListAdapter = new SearchListAdapter();
+
+        protected String doInBackground(String... urls) {
+            StringBuffer jsonHtml = new StringBuffer();
+
+            //서버로 보낼 데이터 설정
+            String search_title = "W_SOB_ID=" + urls[1]
+                    + "&W_ORG_ID=" + urls[2]
+                    + "&W_WORKCENTER_ID=" + urls[3]
+                    + "&W_WORK_ORDER_NO=" + urls[4]
+                   ;
+
+            try {
+                URL obj = new URL("http://" + urls[0] + "/TAIYO/WipOnhandSelect.jsp"); //주소 지정
+
+                HttpURLConnection conn = (HttpURLConnection) obj.openConnection(); //지정된 주소로 연결
+
+                if (conn != null) //
+                {
+                    conn.setReadTimeout(5000);
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("POST"); //메세지 전달 방식 POST로 설정
+                    conn.setDoInput(true);
+                    conn.connect(); //???
+
+                    //서버에 데이터 전달
+                    OutputStream out = conn.getOutputStream();
+                    out.write(search_title.getBytes("UTF-8"));
+                    out.flush();
+                    out.close();
+
+                    if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) //서버에서 응답을 받았을 경우
+                    {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8")); //받은 정보를 버퍼에 저장
+                        while (true) {
+                            String line = br.readLine();
+                            if (line == null) //라인이 없어질때까지 버퍼를 한줄씩 읽음
+                                break;
+                            jsonHtml.append(line);// + "\n");
+                        }
+                        br.close();
+                    }
+                    conn.disconnect();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return jsonHtml.toString(); //결과값 리턴
+        }
+
+
+        protected void onPostExecute(String result) {
+            //페이지 결과값 파싱
+            try {
+                JSONObject RESURT = new JSONObject(result); //JSON 오브젝트 받음
+
+                JSONArray jarrayWorkLevel = RESURT.getJSONArray("RESULT"); //JSONArray 파싱
+
+
+                if (jarrayWorkLevel.length() < 1) {
+                    // Toast.makeText(getApplicationContext(), "데이터가 없습니다.", Toast.LENGTH_SHORT).show();
+                    ScanModify = false;
+                    return;
+                }
+                String chk = "√";
+                for (int j = 0; j < jarrayWorkLevel.length(); j++) {
+                    JSONObject job = jarrayWorkLevel.getJSONObject(j);
+                    if (!job.getString("Status").equals("S")) {
+                        return;
+                    }
+
+                    searchListAdapter.addItem(
+                            handleStringNull(job.getString("OPERATION_DESCRIPTION")),
+                            handleStringNull(job.getString("STEP_DESC")),
+                            handleStringNull(job.getString("WORK_ORDER_NO")),
+                            handleStringNull(job.getString("ITEM_DESCRIPTION")),
+                            handleStringNull(job.getString("TOTAL_ONHAND_QTY")),
+                            handleStringNull(job.getString("OP_POISE_SEQ")),
+                            handleStringNull(job.getString("OP_UNIT_SEQ")),
+                            handleStringNull(job.getString("SORT_NO"))
+
+                    );
+
+                }
+
+                lvInput.setAdapter(searchListAdapter);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String handleStringNull(String input) {
+
+        String rtStr = input.equals("null") ? "" : input;
+
+        return rtStr;
+    }
 
 
 }

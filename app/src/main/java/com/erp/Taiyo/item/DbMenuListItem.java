fabe2051709acd3088ba20a4e811cc592c53c029
manyhun <dahun3441@gmail.com>
2023-11-30 11:09:40 +0900

    풀을 위한 커밋  2311130

---
+++ app/src/main/java/com/erp/Taiyo/item/DbMenuListItem.java
@@ -0,0 +1,46 @@
+package com.erp.Taiyo.item;
+
+public class DbMenuListItem {
+    private String strTopSeq, strTopName, strAuthorityFlag, strUserId, strTopMenuId;
+
+    public String getStrTopSeq() {
+        return strTopSeq;
+    }
+
+    public void setStrTopSeq(String strTopSeq) {
+        this.strTopSeq = strTopSeq;
+    }
+
+    public String getStrTopName() {
+        return strTopName;
+    }
+
+    public void setStrTopName(String strTopName) {
+        this.strTopName = strTopName;
+    }
+
+    public String getStrAuthorityFlag() {
+        return strAuthorityFlag;
+    }
+
+    public void setStrAuthorityFlag(String strAuthorityFlag) {
+        this.strAuthorityFlag = strAuthorityFlag;
+    }
+
+    public String getStrUserId() {
+        return strUserId;
+    }
+
+    public void setStrUserId(String strUserId) {
+        this.strUserId = strUserId;
+    }
+
+    public String getStrTopMenuId() {
+        return strTopMenuId;
+    }
+
+    public void setStrTopMenuId(String strTopMenuId) {
+        this.strTopMenuId = strTopMenuId;
+    }
+}
+
